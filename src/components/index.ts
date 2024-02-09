8e448908b7f3dd11166a0bdd2cce5f4fc07e3a52
axel eldrian hadiwibowo <eldrianaxel1@gmail.com>
2024-02-09 21:48:04 +0700

    app(): add shadcn ui

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import Navbar from "./Navbar";
+import NavItems from "./NavItems";
+import NavItem from "./NavItem";
+import Cart from "./Cart";
+
+export { MaxWidthWrapper, Navbar, NavItems, NavItem, Cart };
