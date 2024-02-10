2d56c6b5a8df2cc9491abb1b1867f562d316ff83
axel eldrian hadiwibowo <eldrianaxel1@gmail.com>
2024-02-10 13:04:21 +0700

    app(MaxWidthWrapper): fix sizing width

--- src/components/MaxWidthWrapper.tsx
+++ src/components/MaxWidthWrapper.tsx
@@ -11,7 +11,10 @@ const MaxWidthWrapper = ({
 }) => {
   return (
     <div
-      className={cn("mx-auto w-full max-w-screen-xl px-2.5 md:px-6", className)}
+      className={cn(
+        "mx-auto w-full max-w-screen-xl px-2.5 sm:px-2.5 md:px-4 lg:px-20",
+        className
+      )}
     >
       {children}
     </div>
