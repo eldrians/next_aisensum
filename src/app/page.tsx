b43b90189691ecd80edba412ec6b6f099eb54596
axel eldrian hadiwibowo <eldrianaxel1@gmail.com>
2024-02-13 22:17:04 +0700

    app(): fix height issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { HeaderSection, CustomerSection } from "@/modules/home-page";
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-4 w-full h-screen justify-center items-center bg-gradient-to-br from-black via-zinc-700 to-black">
+    <main className="flex flex-col gap-4 w-full min-h-screen h-fit py-12 lg:py-0 justify-center items-center bg-gradient-to-br from-black via-zinc-700 to-black">
       <HeaderSection />
       <CustomerSection />
     </main>
