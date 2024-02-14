284dfe9614a736fe5c88a4c8b6f2464628bb190b
axel eldrian hadiwibowo <eldrianaxel1@gmail.com>
2024-02-14 13:28:17 +0700

    app(): change skeleton max height

--- src/modules/home-page/customer-section/table-customer/table-skeleton.tsx
+++ src/modules/home-page/customer-section/table-customer/table-skeleton.tsx
@@ -2,7 +2,7 @@ import * as UI from "@/components/ui";
 
 const TableSkeleton = () => {
   return (
-    <div className="mt-4 flex flex-col min-h-[420px] gap-4 items-center">
+    <div className="mt-4 flex flex-col min-h-[320px] gap-4 items-center">
       <div className="flex flex-row w-full gap-4">
         <UI.skeleton.Skeleton className="w-5/6 h-[40px]" />
         <UI.skeleton.Skeleton className="w-1/6 h-[40px]" />
